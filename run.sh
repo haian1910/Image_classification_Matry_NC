4de51ff72c0486b6c89e6b177f81f9c9bc658ec8
phunghuy159 <phunggiahuy.15092005@gmail.com>
2025-08-27 16:14:53 +0000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Set up environment variables for distributed training
+export PYTHONPATH="/workspace/Image_classification_Matry_NC"
+export LOCAL_RANK='-1'
+export MASTER_ADDR='127.0.0.1'
+export MASTER_PORT='12355'
+export RANK='0'
+export WORLD_SIZE='1'
+
+python tools/train.py \
+    --model matryoshka_resnet18 \
+    --dataset cifar10 \
+    --kd nc1 \
+    --teacher-model resnet101 \
+    --teacher-pretrained \
+    --matryoshka-dims 128 256 384 512 \
+    --matryoshka-adaptive \
+    --epochs 1 \
+    --decay-rate 0.1 \
+    --experiment matryoshka_nc1_test_50ep
