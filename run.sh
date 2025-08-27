89707de67ee88dfb20cecb1db167efd04900a31d
haiancol <145516487+haian1910@users.noreply.github.com>
2025-08-28 00:26:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,10 +11,11 @@ python tools/train.py \
     --model matryoshka_resnet18 \
     --dataset cifar10 \
     --kd nc1 \
-    --teacher-model resnet101 \
+    --teacher-model resnet101 \/workspace/Image_classification_Matry_NC/experiments/exp/checkpoint-0.pth.tar
     --teacher-pretrained \
     --matryoshka-dims 128 256 384 512 \
     --matryoshka-adaptive \
     --epochs 1 \
     --decay-rate 0.1 \
     --experiment matryoshka_nc1_test_50ep
+# python tools/train.py -c configs/strategies/resnet/resnet.yaml --model resnet101
